cc516795db4f4c6ae610ffb548200a1363f5abd9
Alex Fleming <alex@alexfleming.dev>
2020-06-08 14:35:04 +0100

    Fix ignore and add aws-exports.js

---
+++ src/aws-exports.js
@@ -0,0 +1,9 @@
+const awsConfig = {
+  Auth: {
+    region: "eu-west-1",
+    userPoolId: "eu-west-1_3GiYISyAa",
+    userPoolWebClientId: "1nju546up851bg9prvbbhpt67c"
+  }
+};
+
+export default awsConfig;
